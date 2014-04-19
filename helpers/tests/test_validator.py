2c131dd5304b013819fb75f326f92cd5c1cd7d9d
EmanuelaMollova <emanuela.mollova@gmail.com>
2014-04-20 01:39:34 +0300

    Improve the validator class and change its tests

--- helpers/tests/test_validator.py
+++ helpers/tests/test_validator.py
@@ -8,54 +8,53 @@ import unittest
 class ValidatorTest(unittest.TestCase):
 
     def setUp(self):
-        self.validator = Validator()
+        self.v = Validator()
 
     def test_validate_name_returns_none_on_valid_name(self):
-        self.assertEqual(None, self.validator.validate_name(2, 'aa'))
-        self.assertEqual(None, self.validator.validate_name(0, 'aa'))
-        self.assertEqual(None, self.validator.validate_name(1, 'a'))
+        self.assertEqual(None, self.v.validate_name(2, 'aa'))
+        self.assertEqual(None, self.v.validate_name(0, 'aa'))
+        self.assertEqual(None, self.v.validate_name(1, 'a'))
 
     def test_validate_name_raises_exception_on_invalid_name(self):
-        self.assertRaises(Exception, self.validator.validate_name, 2, 'a')
-        self.assertRaises(Exception, self.validator.validate_name, 1, '')
+        self.assertRaises(Exception, self.v.validate_name, 2, 'a')
+        self.assertRaises(Exception, self.v.validate_name, 1, '')
 
     def test_validate_number_returns_none_on_valid_number(self):
-        self.assertEqual(None, self.validator.validate_number(1, 1))
-        self.assertEqual(None, self.validator.validate_number(0, 0))
-        self.assertEqual(None, self.validator.validate_number(0, 10))
-        self.assertEqual(None, self.validator.validate_number(0, 3.56))
-        self.assertEqual(None, self.validator.validate_number(0, 0.01))
+        self.assertEqual(None, self.v.validate_number(1, 1, ''))
+        self.assertEqual(None, self.v.validate_number(0, 0, ''))
+        self.assertEqual(None, self.v.validate_number(0, 10, ''))
+        self.assertEqual(None, self.v.validate_number(0, 3.56, ''))
+        self.assertEqual(None, self.v.validate_number(0, 0.01, ''))
 
     def test_validate_number_raises_exception_on_invalid_number(self):
-        self.assertRaises(Exception, self.validator.validate_number, 2, 1)
-        self.assertRaises(Exception, self.validator.validate_number, 0, -1)
-        self.assertRaises(Exception, self.validator.validate_number, 1, 0)
-        self.assertRaises(Exception, self.validator.validate_number, 2, 1.99)
+        self.assertRaises(Exception, self.v.validate_number, 2, 1, '')
+        self.assertRaises(Exception, self.v.validate_number, 0, -1, '')
+        self.assertRaises(Exception, self.v.validate_number, 1, 0, '')
 
     def test_validate_number_works_with_strings(self):
-        self.assertEqual(None, self.validator.validate_number(1, '1'))
-        self.assertEqual(None, self.validator.validate_number(0, '0'))
-        self.assertEqual(None, self.validator.validate_number(0, '10'))
-        self.assertEqual(None, self.validator.validate_number(0, '0.001'))
+        self.assertEqual(None, self.v.validate_number(1, '1', ''))
+        self.assertEqual(None, self.v.validate_number(0, '0', ''))
+        self.assertEqual(None, self.v.validate_number(0, '10', ''))
+        self.assertEqual(None, self.v.validate_number(0, '0.001', ''))
 
-        self.assertRaises(Exception, self.validator.validate_number, 2, '1')
-        self.assertRaises(Exception, self.validator.validate_number, 0, '-1')
-        self.assertRaises(Exception, self.validator.validate_number, 1, '0')
-        self.assertRaises(Exception, self.validator.validate_number, 1, '0.76')
+        self.assertRaises(Exception, self.v.validate_number, 2, '1', '')
+        self.assertRaises(Exception, self.v.validate_number, 0, '-1', '')
+        self.assertRaises(Exception, self.v.validate_number, 1, '0', '')
+        self.assertRaises(Exception, self.v.validate_number, 1, '0.76', '')
 
     def test_validate_returns_none_on_valid_parameter(self):
         lambda_function = lambda x: x < 10
-        self.assertEqual(None, self.validator.validate(lambda_function, 2, ''))
+        self.assertEqual(None, self.v.validate(lambda_function, 2, ''))
 
         lambda_function = lambda x: x ** 3 == 8
-        self.assertEqual(None, self.validator.validate(lambda_function, 2, ''))
+        self.assertEqual(None, self.v.validate(lambda_function, 2, ''))
 
     def test_validate_raises_exception_on_invalid_parameter(self):
         lf = lambda x: x < 10
-        self.assertRaises(Exception, self.validator.validate, lf, 20, '')
+        self.assertRaises(Exception, self.v.validate, lf, 20, '')
 
         lf = lambda x: x ** 3 == 8
-        self.assertRaises(Exception, self.validator.validate, lf, 20, '')
+        self.assertRaises(Exception, self.v.validate, lf, 20, '')
 
 
 if __name__ == '__main__':
--- helpers/validator.py
+++ helpers/validator.py
@@ -4,13 +4,13 @@ class Validator():
             message = "The name should be at least {} symbols"
             raise Exception(message.format(min_length))
 
-    def validate_number(self, min_value, parameter):
+    def validate_number(self, min_value, parameter, parameter_name):
         if isinstance(parameter, str):
             parameter = float(parameter)
 
         if parameter < min_value:
             message = "The {} should be at least {}"
-            raise Exception(message.format(parameter.__name__, min_value))
+            raise Exception(message.format(parameter_name, min_value))
 
     def validate(self, condition, parameter, message):
         if not condition(parameter):
